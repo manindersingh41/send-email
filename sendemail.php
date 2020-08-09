65ec9c69b351099a49a38551e7158df7391d132d
manindersingh41 <60435413+manindersingh41@users.noreply.github.com>
2020-08-09 12:50:18 +0530

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,13 +1,14 @@
 <?php
 
 //Recipient
+$from = 'monu642858@gmail.com';
 $to = 'manii.s4141@gmail.com';
 //Subject
 $subject = 'This is our test email';
 //Message
 $message = '<h1>Hi there.</h1><p>Thanks for testing</p>';
 //Headers
-$headers = "From: The Sender Name <sender@monu642858@gmail.com\r\n";
+$headers = "From:".$from;
 $headers .= "Reply: replyto@monu642858@gmail.com\r\n";
 $headers .= "Content-type: text/html\r\n";
 
