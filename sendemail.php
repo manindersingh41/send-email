0f03214a8d90f871b5e1330067bf32b8b3b525a7
manindersingh41 <60435413+manindersingh41@users.noreply.github.com>
2020-08-09 12:47:16 +0530

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,16 @@
+<?php
+
+//Recipient
+$to = 'manii.s4141@gmail.com';
+//Subject
+$subject = 'This is our test email';
+//Message
+$message = '<h1>Hi there.</h1><p>Thanks for testing</p>';
+//Headers
+$headers = "From: The Sender Name <sender@monu642858@gmail.com\r\n";
+$headers .= "Reply: replyto@monu642858@gmail.com\r\n";
+$headers .= "Content-type: text/html\r\n";
+
+// Send email
+mail($to, $subject, $message, $headers );
+?>
